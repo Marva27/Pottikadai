d1a89d889a3da3016d9a22a81a9333151cfee656
Marva27 <srinimarva@gmail.com>
2021-10-25 19:49:46 -0500

    Add files via upload

---
+++ src/main/java/com/pottikadai/www/PottiKadaiApplication.java
@@ -0,0 +1,13 @@
+package com.pottikadai.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PottiKadaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PottiKadaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pottikadai/www/PottiKadaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pottikadai.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PottiKadaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
